d6fca884f4a597fb3472bd6c6577f803aad271e1
TFSID <tegar.firman215@gmail.com>
2026-07-27 09:51:02 +0700

    update labs environment

--- labs_env/env_vuln/entrypoint.sh
+++ labs_env/env_vuln/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /app
 python3 server.py &>/dev/null &
-exec nginx -g 'daemon off;'
+# setarch -R disables ASLR for the launched process (deterministic addresses)
+exec setarch x86_64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
